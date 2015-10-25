e2a01d408db7dd1c8855e2d6d0a540b142f0f9cc
Martin Felke <martin.felke@googlemail.com>
2015-10-25 09:41:32 +0100

    small fix for ui description of mass_threshold_factor

--- source/blender/makesrna/intern/rna_modifier.c
+++ source/blender/makesrna/intern/rna_modifier.c
@@ -5212,7 +5212,7 @@ static void rna_def_modifier_fracture(BlenderRNA *brna)
 	RNA_def_property_float_sdna(prop, NULL, "mass_threshold_factor");
 	RNA_def_property_range(prop, 0.0f, 1.0f);
 	RNA_def_property_ui_text(prop, "Stability Factor",
-	                         "Determines how 'stable' an object is, by multiplying threshold * mass * this factor, 0 means disabled, 1 means highest mass					                          dependent stability");
+	                         "Determines how 'stable' an object is, by multiplying threshold * mass * this factor, 0 means disabled, 1 means highest mass dependent stability");
 	RNA_def_property_clear_flag(prop, PROP_ANIMATABLE);
 	RNA_def_property_update(prop, 0, "rna_Modifier_update");
 }
