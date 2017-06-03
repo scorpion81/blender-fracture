b25edc6352e097df41d438c15799632ae883bcbd
Martin Felke <martin.felke@googlemail.com>
2017-06-03 19:58:00 +0200

    set allowed smoke timescale from 0.0 to 2.0, thus allow static and a bit faster smoke as well

--- source/blender/makesrna/intern/rna_smoke.c
+++ source/blender/makesrna/intern/rna_smoke.c
@@ -585,8 +585,8 @@ static void rna_def_smoke_domain_settings(BlenderRNA *brna)
 
 	prop = RNA_def_property(srna, "time_scale", PROP_FLOAT, PROP_NONE);
 	RNA_def_property_float_sdna(prop, NULL, "time_scale");
-	RNA_def_property_range(prop, 0.2, 1.5);
-	RNA_def_property_ui_range(prop, 0.2, 1.5, 0.02, 5);
+	RNA_def_property_range(prop, 0.0, 2.0);
+	RNA_def_property_ui_range(prop, 0.0, 2.0, 0.02, 5);
 	RNA_def_property_ui_text(prop, "Time Scale", "Adjust simulation speed");
 	RNA_def_property_update(prop, NC_OBJECT | ND_MODIFIER, "rna_Smoke_resetCache");
 
